e64daee69e5c19d8f009821ba04fde048b694e2f
zhorton <zak.horton@vml.com>
2020-05-31 12:08:20 -0500

    Added devDependency

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let fs = require('fs');
 let mix = require('laravel-mix')
 
-const str = (value = '') => require('laravel-js-str').Str.of(value);
 
 const CollectFiles = (folder, files = []) => {
+    const str = (value = '') => require('laravel-js-str').Str.of(value);
+
     const isFolder = to => new File(path.resolve(to)).isDirectory();
     const CombineFiles = (Files, Segments = []) => [ ...Files, path.join(Mix.paths.rootPath, Segments[0], '/', Segments[1])];
 
